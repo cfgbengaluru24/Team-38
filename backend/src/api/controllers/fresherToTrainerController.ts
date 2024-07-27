90181e35f36cf6071c4a680b62da6e87dda9f1b9
Pulkit Raina <pulkitraina2608@gmail.com>
2024-07-28 03:07:37 +0530

    front end changes

--- backend/src/api/controllers/fresherToTrainerController.ts
+++ backend/src/api/controllers/fresherToTrainerController.ts
@@ -18,6 +18,32 @@ export const freshersDetails = async (req: Request, res: Response) => {
 	}
 };
 
+export const specificFresher = async (req: Request, res: Response) => {
+	const { userRole } = req;
+	if(userRole !== "advanced") return res.status(403).json({msg: "you are not authorized!"});
+	const { id } = req.params;
+
+	try {
+		const response = await prisma.fresherModules.findMany({
+			where: {
+				fresherId: id
+			},
+			include: {
+				Modules: true
+			}
+		});
+
+		console.log("RESPONSE IS LMAOOO: ", response);
+		
+
+		if(!response) throw new Error("No user found!");
+		res.status(200).json(response);
+	}
+	catch(e: any) {
+		return res.status(500).json({msg: "error! " + e.message});
+	}
+}
+
 export const customersDetails = async (req: Request, res: Response) => {
 	const { userRole, id } = req;
 
