c7d5b6883b1576e7cca9e6b5e7a0db00317c4531
SivananthamDuraivel <210701250@rajalakshmi.edu.in>
2024-07-28 00:36:32 +0530

    back to

--- backend/src/api/routes/advanced.ts
+++ backend/src/api/routes/advanced.ts
@@ -7,7 +7,6 @@ import {
 	customersDetails,
 	freshersDetails,
 } from "../controllers/fresherToTrainerController";
-import { writeQuestions } from "../controllers/writeQuestionsController";
 
 // Endpoint here hits the /api/a/ endpoint
 
@@ -15,16 +14,10 @@ export const api = express();
 
 
 api.use(authMiddleware);
-<<<<<<< HEAD
-api.post("/signup", signup);
-api.post("/freshers", freshersDetails);
-api.get("/customers", customersDetails);
-=======
 api.post("/signup", signup);							// Tranfer fresher to advanced
 api.post("/freshers", freshersDetails);		// Fetch all fresher details
 api.get("/customers", customersDetails); 	// Fetch all customers under this guy
 
->>>>>>> 8636cc6c86aac5c25a854f02add24604f342140c
 
 // api.get("/", getAllTeachers);
 // api.get("/scores", getClassScores);
