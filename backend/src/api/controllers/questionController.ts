c61d76ce8834a0315b6c04f8335cdb66d0afdce8
Pulkit Raina <pulkitraina2608@gmail.com>
2024-07-27 20:15:37 +0530

    ansering questions

--- backend/src/api/controllers/questionController.ts
+++ backend/src/api/controllers/questionController.ts
@@ -1,10 +1,14 @@
 import { Request,Response } from "express"
 
-export const fetchQuestions = async(req:Request,res:Response)=>{
+export const fetchQuestions = async(req: Request, res: Response)=>{
     try{
         
     }catch(err)
     {
         console.log("question fetch catch : ",err)
     }
+}
+
+export const enterQuestions = async (req: Request, res: Response) => {
+    return res.status(200).json({msg: "Sucess"});
 }
