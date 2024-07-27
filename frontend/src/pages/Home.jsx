d3cdc5342dd461f9700f7cbccc6cf8e72341c6b7
Lekhana <lekhanapatel6@gmail.com>
2024-07-27 19:23:27 +0530

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import woman from "../assets/woman1.jpg"
 const Home = () => {
   return (
@@ -9,8 +10,8 @@ const Home = () => {
         <div className="left text-center md:text-left">
           <h2 className="text-xl md:text-2xl font-bold mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, eum.</h2>
           <p className="mb-4">Lorem ipsum dolor sit amet.</p>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded mb-2 md:mb-0 md:mr-2">Trainee</button>
-          <button className="bg-green-500 text-white px-4 py-2 rounded">Trainer</button>
+          <Link to='TrainerSignin'><button className="bg-blue-500 text-white px-4 py-2 rounded mb-2 md:mb-0 md:mr-2">Trainee</button></Link>
+          <Link></Link><button className="bg-green-500 text-white px-4 py-2 rounded">Trainer</button>
         </div>
       </div>
     </div>
